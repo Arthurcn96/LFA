08430950bea0b7ad14f4f7cfdd9d462ea6e56335
usuario <usuario@192.168.1.168>
2017-06-26 14:43:37 -0300

    Parte dos tokens.

--- AnalisadorLexico/src/analisadorlexico/Constantes.java
+++ AnalisadorLexico/src/analisadorlexico/Constantes.java
@@ -5,14 +5,31 @@ public interface Constantes {
     enum Token { NUM, ATRIB, IDENT, EOF, PTOVIRG, VAZIO };
  
     String     DIGITOS    = "0123456789",
-                LETRAS    = "abcdefghijklmnopqrstuvwxyz",
+                LETRAS    = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOQRSTUVWXYZ",
                 ATRIB      = "-:",
-                VAZIOS    = " \r\n\t";
+                VAZIOS    = " \r\n\t",
+                OPBINARIO = "*/%<>",//aqui tinha os operadores <= >= == && ||
+                OPBINUNARIO = "+-",
+                OPUNARIO = "!%",
+                WHILE = "while",
+                DO = "do",
+                IF = "if",
+                SWITCH = "switch",
+                CASE = "case";
  
     char    EOF    = 0,
             HIFEN     = '-',
             PTOVIRG    = ';',
-            DOISPONTOS     = ':';
+            DOISPONTOS     = ':',
+            OPIGUAL = '=',
+            ABREASPAS = '"',
+            FECHAASPAS = '"',
+            ABRECHAVES = '{',
+            FECHACHAVES = '}',
+            ABREPAR = '(',
+            FECHAPAR = ')';
+            
  
     String   NOME_DEFAULT_ARQUIVO_ENTRADA = "entrada.txt";
+    
 }
